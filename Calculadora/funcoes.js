5d328db9f9917184026e6348882b42cd93da15f6
Lucas Leandro <163912927+Lucas5812@users.noreply.github.com>
2024-03-20 10:48:05 -0300

    Calculadora adicionada
    Adicionei os arquivos da calculadora, uma atividade passada por Dudu, os botões numéricos são um projeto em desenvolvimento, porém as funções de somar, subtrair, multiplicar e dividir estão funcionando como deviam.

---
+++ Calculadora/funcoes.js
@@ -0,0 +1,46 @@
+/*Bloco de funções matemáticas*/
+function somar() {
+    var r1 = parseInt (document.getElementById("v1").value);
+    var r2 = parseInt (document.getElementById("v2").value);
+    var resposta = r1 + r2;
+    document.getElementById("res").innerHTML = "Resposta:  " + resposta;
+}
+function subtrair() {
+    var r1 = parseInt (document.getElementById("v1").value);
+    var r2 = parseInt (document.getElementById("v2").value);
+    var resposta = r1 - r2;
+    document.getElementById("res").innerHTML = "Resposta:  " + resposta;
+}
+function dividir() {
+    var r1 = parseInt (document.getElementById("v1").value);
+    var r2 = parseInt (document.getElementById("v2").value);
+    var resposta = r1 / r2;
+    document.getElementById("res").innerHTML = "Resposta:  " + resposta;
+}
+function multiplicar() {
+    var r1 = parseInt (document.getElementById("v1").value);
+    var r2 = parseInt (document.getElementById("v2").value);
+    var resposta = r1 * r2;
+    document.getElementById("res").innerHTML = "Resposta:  " + resposta;
+}
+
+/*Bloco de adição de numeros */
+var ultvselecionado = "r1"
+function add(numero){
+    var r1 = parseInt (document.getElementById("v1").value);
+    var r2 = parseInt (document.getElementById("v2").value);
+    
+    if (ultvselecionado == "r1"){
+        r1.value += numero
+    }
+    else if (ultvselecionado == "r2"){
+        r2.value += numero
+    }
+}
+
+document.getElementById("v1").addEventListener("click", function(){
+    ultvselecionado = "r1";
+});
+document.getElementById("v2").addEventListener("click", function(){
+    ultvselecionado = "r2";
+})
