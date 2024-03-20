bf7deb69d1cecbf6bb6475363ba57c52c295d4c3
Lucas Leandro <163912927+Lucas5812@users.noreply.github.com>
2024-03-20 11:24:25 -0300

    Botões numericos funcionando
    os botões numéricos que tem embaixo dos painéis agora deve funcionar corretamente, para escolher qual painel receberá os números basta clickar no desejado

--- Calculadora/funcoes.js
+++ Calculadora/funcoes.js
@@ -25,16 +25,18 @@ function multiplicar() {
 }
 
 /*Bloco de adição de numeros */
-var ultvselecionado = "r1"
+var ultvselecionado = "v1"
 function add(numero){
-    var r1 = parseInt (document.getElementById("v1").value);
-    var r2 = parseInt (document.getElementById("v2").value);
+    var r1 = document.getElementById("v1").value;
+    var r2 = document.getElementById("v2").value;
     
     if (ultvselecionado == "r1"){
-        r1.value += numero
+        r1 += numero
+        document.getElementById("v1").value =  r1;
     }
     else if (ultvselecionado == "r2"){
-        r2.value += numero
+        r2 += numero
+        document.getElementById("v2").value = r2;
     }
 }
 
