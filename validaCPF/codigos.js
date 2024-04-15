8eae6b323edd6c3b079b47e05022bb6f5b76985b
Lucas Leandro <163912927+Lucas5812@users.noreply.github.com>
2024-04-14 23:22:57 -0300

    Valida CPF completo
    Simplesmente o maior projeto que ja fizemos, ainda tem alguns erros que estamos cientes(111.111.111-11 e em diante são considerados cpfs "validos"), mas não sabemos como corrigir ainda, agracemos a compreensão

--- validaCPF/codigos.js
+++ validaCPF/codigos.js
@@ -5,80 +5,149 @@ var valor1 = 0
 var valor2 = 0
     
 function VerificaCPF(CPF){
-    var cpf = document.getElementById("CPF").value; //converte o input recebido no html para uma variavel em js
+    var cpf = document.getElementById("CPF").value;
 
     document.getElementById("veredicto").innerHTML = "..."
-    //console.log("Função iniciada")
+    console.log("Função iniciada")
 
-    if (/^\d+$/.test(cpf) && cpf.length === 11){ //verifica se o valor recebido são digitos de 0-9, especificado na expressão "/^\d+$/" e se o valor tem mais que 11 digitos
+    if (/^\d+$/.test(cpf) && cpf.length === 11){
             cpfVerificado = cpf
-            //console.log("CPF verificado") 
-            trataCPF1(cpfVerificado)     //chama a função trataCPF1 fornecendo o CPF verificado como parâmetro
+            console.log("CPF verificado") 
+            trataCPF1(cpfVerificado)
+            aparecesite();
     }
 
-    else { //caso não atenda alguma das condições, o usuário recebe uma mensagem informando que o valor está incorreto
+    else {
         document.getElementById("veredicto").innerHTML = "tamanho inválido, um CPF por padrão deve ter 11 digitos"
     }
         
     }
 
-
-
-function trataCPF1(cpf){ //engloba os preparativos e a verificação do primeiro digito indentificador de um cpf
-    for (let i = 0; i <= 10; i++){ 
-       let numseparado = cpf.substring(i, i+1) //separa cada numero do cpf e põe nessa variavel
-        //console.log(numseparado)
-        cpfseparado[i] = numseparado //atribui o valor da variavel a um espaço na array, no primeiro numero do contador, logo o primeiro número do cpf é adicionado no "i", que é 0, e o valor vai aumentando conforme o contador aumenta
+function trataCPF1(cpf){
+    for (let i = 0; i <= 10; i++){
+       let numseparado = cpf.substring(i, i+1)
+        console.log(numseparado)
+        cpfseparado[i] = numseparado
         document.getElementById("veredicto").innerHTML = "Seu cpf está sendo analisado, aguarde..."
     }
 
     console.log("valores do cpf separados")
 
-    for (let i = 0; i <= 8; i++){ //executa os passos para a verificação do primeiro digito indentificador
-        cpfcalculado[i] = cpfseparado[i] * (10 - i) //os passos consistem em multiplicar os valores por um dado numero, as instruções exatas são encontrados no site da receita federal
+    for (let i = 0; i <= 8; i++){
+        cpfcalculado[i] = cpfseparado[i] * (10 - i)
         console.log(cpfcalculado[i])
     }
     valor1 = cpfcalculado[0] + cpfcalculado[1] + cpfcalculado[2] + cpfcalculado[3] + cpfcalculado[4] + cpfcalculado[5] + cpfcalculado[6] + cpfcalculado[7] + cpfcalculado[8]
-    //console.log(valor1) //soma todos os resultados obtidos até o momento
-    valor2 = (valor1*10)%11 //multiplica por 10 e então divide por 11, e obtém o resto
-    //console.log(valor2)
-    if(valor2 === 10 || valor2 === 11){ //caso o resto seja 10 ou 11, o valor é subsituido por 0
+    console.log(valor1)
+    valor2 = (valor1*10)%11
+    console.log(valor2)
+    if(valor2 === 10 || valor2 === 11){
        valor2 = 0
-       //console.log("digito de valor2 alterado para:")
-       //console.log(valor2)
-       //console.log(cpfseparado[9])
+       console.log("digito de valor2 alterado para:")
+       console.log(valor2)
+       console.log(cpfseparado[9])
      }
     document.getElementById("veredicto").innerHTML = "Verificando primeiro digito indentificador..."
-     if(valor2 == cpfseparado[9]){ //checa se os valores calculados anteriormente são os mesmos encontrados no primeiro digito indentificador
+     if(valor2 == cpfseparado[9]){
          document.getElementById("veredicto").innerHTML = "primeiro indentificador verificado!"
          trataCPF2()
      }
-     else{ //caso não seja, o usuário recebe este aviso
+     else{
             document.getElementById("veredicto").innerHTML = "Cpf Inválidado no primeiro indentificador!"
     }
 }
-
-
-
-function trataCPF2(){ //como os preparativos ja foram feitos, apenas altera alguns valores e faz a verificação do segundo digito indentificador
+function trataCPF2(){
     console.log("recebeu cpf tratado")
     for (let i = 0; i <= 9; i++){
-        cpfcalculado[i] = cpfseparado[i] * (11 - i) //similar ao caso do tratacpf1, contudo o numero a ser multiplicado inicia com 11 e vai até o 10º numero do cpf
-        //console.log(cpfcalculado[i])
+        cpfcalculado[i] = cpfseparado[i] * (11 - i)
+        console.log(cpfcalculado[i])
     }
-    valor1 = cpfcalculado[0] + cpfcalculado[1] + cpfcalculado[2] + cpfcalculado[3] + cpfcalculado[4] + cpfcalculado[5] + cpfcalculado[6] + cpfcalculado[7] + cpfcalculado[8] + cpfcalculado[9] //similar ao caso anterior
+    valor1 = cpfcalculado[0] + cpfcalculado[1] + cpfcalculado[2] + cpfcalculado[3] + cpfcalculado[4] + cpfcalculado[5] + cpfcalculado[6] + cpfcalculado[7] + cpfcalculado[8] + cpfcalculado[9]
     valor2 = (valor1*10)%11
     if(valor2 === 10 || valor2 === 11){
         valor2 = 0
-        //console.log("digito de valor2 alterado para:")
-        //console.log(valor2)
-        //console.log(cpfseparado[10])
+        console.log("digito de valor2 alterado para:")
+        console.log(valor2)
+        console.log(cpfseparado[10])
       }
-    if(valor2 == cpfseparado[10]){ //checa se os valores calculados anteriormente são os mesmos encontrados no segundo digito indentificador
-        document.getElementById("veredicto").innerHTML = "CPF valido!"
+    if(valor2 == cpfseparado[10]){
+        document.getElementById("veredicto").innerHTML = "CPF valido!";
     }
-    else{ //caso não seja, o usuário recebe este aviso
+    else{
            document.getElementById("veredicto").innerHTML = "Cpf Inválidado no segundo indentificador!"
    }
 
+}
+
+function limpa_formulário_cep() {
+    document.getElementById('rua').value=("");
+    document.getElementById('bairro').value=("");
+    document.getElementById('cidade').value=("");
+    document.getElementById('uf').value=("");
+    document.getElementById('ibge').value=("");
+}
+
+function meu_callback(conteudo) {
+if (!("erro" in conteudo)) {
+    document.getElementById('rua').value=(conteudo.logradouro);
+    document.getElementById('bairro').value=(conteudo.bairro);
+    document.getElementById('cidade').value=(conteudo.localidade);
+    document.getElementById('uf').value=(conteudo.uf);
+    document.getElementById('ibge').value=(conteudo.ibge);
+}
+else {
+
+    limpa_formulário_cep();
+    document.getElementById("cepfailed").innerHTML = "Oops, parece que seu CEP não existe, digite novamente!";
+}
+}
+
+function pesquisacep(valor) {
+
+var cep = valor.replace(/\D/g, '');
+if (cep != "") {
+var validacep = /^[0-9]{8}$/;
+
+    if(validacep.test(cep)) {
+
+        document.getElementById('rua').value="...";
+        document.getElementById('bairro').value="...";
+        document.getElementById('cidade').value="...";
+        document.getElementById('uf').value="...";
+        document.getElementById('ibge').value="...";
+
+        var script = document.createElement('script');
+        script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+        document.body.appendChild(script);
+
+    } 
+    else {
+        
+        limpa_formulário_cep();
+        document.getElementById("cepfailed").innerHTML = "Formato inválido, digite seu CEP novamente.";
+    }
+}
+else {
+    limpa_formulário_cep();
+}
+}
+
+function verificaCampos() {
+    var cpf = document.getElementById("CPF").value;
+    var cep = document.getElementById("cep").value;
+
+    if (/^\d+$/.test(cpf) && cpf.length === 11 && validarCEP(cep)) {
+        document.getElementById("veredicto").innerHTML = "...";
+        trataCPF1(cpf);
+        pesquisacep(cep);
+        aparecesite(); // Chamando a função aparecesite() apenas se o CPF e o CEP estiverem corretos
+    } else {
+        document.getElementById("veredicto").innerHTML = "CPF inválido ou CEP inválido!";
+        document.getElementById("oculto").style.display = "none"; // Oculta as informações ocultas se os campos estiverem incorretos
+    }
+}
+
+function aparecesite() {
+    document.getElementById("oculto").style.display = "block";
+    document.getElementById("oculto").style.color = "rgb(188, 53, 53)";
 }
